11f8b3ba821439c74dd6b17b748a83ced618cc4d
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-26 17:10:00 -0400

    Add Do Now

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1. Declare a variable called name. 
+//  - Give it the value of your name. 
+//  - This is a string so the value should be in quotes ""
+
+
+
+// 2. Declare a variable called age. 
+//  - Give it the value of your age. 
+//  - This is a number, so the value does not have to be in quotes
+
+// 3. Declare a variable called answer.
+//  - This should save the div with a class of `answer`
+
+
+// 4. When you're done, remove the comment from the code below.
+
+// answer.innerHTML = name + " is " + age;
