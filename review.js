0a0a567c04ab3739c66c679a9386c7ff39c1d234
Replit user <>
2023-11-13 16:10:23 +0000

    completed all tasks

--- review.js
+++ review.js
@@ -3,22 +3,27 @@
 
 // 2. Take look at the HMTL. When the user clicks the "option-one" button, we want the "option-one-screen" div to show. 
 //  - First, store those elements as variables. Can someone take me through the steps to do that?
-
+const optionOneButton = document.querySelector(".option-one");
+const optionOneScreen = document.querySelector(".option-one-screen");
 
 // CODE SOLO, PART 1
 // 3. Declare a variable to save the button with the class "option-two"
 // 4. Declare a variable to save the div with the class "option-two-screen"
+const optionTwoButton = document.querySelector(".option-two");
+const optionTwoScreen = document.querySelector(".option-two-screen");
 
 
 
 // CODE ALONG, PART 2
 // 5. Let's use the console to check our variables saved correctly. 
 //  - Watch as I use console.log() to check the contents of our option-one variables.
+// console.log(optionOneScreen);
 
 
 // CODE SOLO, PART 2
 // 6. Now use console.log() to check the contents of the option-two variables you created. 
 //  - Did they save correctly? If not, fix them!
+// console.log(optionTwoScreen);
 
 
 
@@ -27,14 +32,11 @@
 // 7. Plug in the variables we created to make it so that parts of the story show up when the user clicks the button! 
 //  - Uncomment the code, try it out on the webpage, and see what happens!
 
-/* Do not worry about the code below until the end! 
 
- INSERT VARIABLE FOR OPTION ONE BUTTON.addEventListener("click", function(){
-    INSERT VARIABLE FOR OPTION-ONE-SCREEN DIV.style.display="block";
+optionOneButton.addEventListener("click", function(){
+    optionOneScreen.style.display="block";
 });
 
- INSERT VARIABLE FOR OPTION TWO BUTTON.addEventListener("click", function(){
-    INSERT VARIABLE FOR OPTION-TWO-SCREEN DIV.style.display="block";
+optionTwoButton.addEventListener("click", function(){
+	optionTwoScreen.style.display="block";
 });
-
-*/
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // 1. Declare a variable called myName. 
 //  - Give it the value of your name. 
 //  - This is a string so the value should be in quotes ""
-
+const myName = "Vincent";
 
 
 // 2. Declare a variable called age. 
 //  - Give it the value of your age. 
 //  - This is a number, so the value does not have to be in quotes
+const age = 17;
 
 // 3. Declare a variable called answer.
 //  - This should save the div with a class of `answer`
-
+const answer = document.querySelector(".answer");
+// console.log(answer);
 
 // 4. When you're done, remove the comment from the code below.
 
-// answer.innerHTML = name + " is " + age;
+answer.innerHTML = myName + " is " + age;
